cf63b3ea9bd2fe3d2148a4b5edd5e20de09bd0f4
Roma Bulani <romabulani@gmail.com>
2022-04-23 22:18:35 +0530

    feat: context for quiz

--- src/contexts/types/index.ts
+++ src/contexts/types/index.ts
@@ -1,1 +1,2 @@
 export * from "./authContext.type";
+export * from "./quizContext.type";
---
+++ src/contexts/types/quizContext.type.ts
@@ -0,0 +1,50 @@
+export type QuestionAnswersObj = {
+  statement: string;
+  options: Array<string>;
+};
+
+export type QuizQuestionAndAnswers = Array<QuestionAnswersObj>;
+
+export type QuizType = {
+  category: string;
+  selectedAnswers: Array<string>;
+  score: number;
+  activeQuestion: number;
+  activeQuiz: QuizQuestionAndAnswers;
+  activeQuizAnswers: Array<string>;
+};
+
+export type QuizActionType =
+  | {
+      type: "SET_CATEGORY";
+      payload: string;
+    }
+  | {
+      type: "SET_ACTIVE_QUIZ";
+      payload: QuizQuestionAndAnswers;
+    }
+  | {
+      type: "SET_ANSWERS";
+      payload: string;
+    }
+  | {
+      type: "SET_SCORE";
+      payload: number;
+    }
+  | {
+      type: "SET_ACTIVE_QUESTION";
+      payload: number;
+    }
+  | {
+      type: "SET_ACTIVE_QUIZ_ANSWERS";
+      payload: Array<string>;
+    }
+  | {
+      type: "RESET_ANSWERS";
+    };
+
+export type QuizContextType = {
+  quizState: QuizType;
+  quizDispatch: (arg0: QuizActionType) => void;
+  initialQuizState: QuizType;
+};
